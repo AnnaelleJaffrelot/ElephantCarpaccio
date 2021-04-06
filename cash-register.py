a058d66b9649f6401ca1af988d645382bc2fbae4
dreyn <robz52@free.fr>
2021-04-06 09:08:52 +0200

    Étape 2 de notre backlog

--- cash-register.py
+++ cash-register.py
@@ -1,1 +1,17 @@
-print("Hello World !")
+import numpy as np
+
+countries = np.array(["Allemagne ", "Autriche ", "Belgique ", "Bulgarie ", "Chypre ", "Croatie ", "Danemark ", "Espagne ",
+	"Estonie ", "Finlande ", "France ", "Grèce ", "Hongrie ", "Irlande ", "Italie ", "Lettonie ",
+	"Lituanie ", "Luxembourg ", "Malte ", "Pays-Bas ", "Pologne ", "Portugal ", "République Tchèque ",
+	"Roumanie ", "Royaume-Uni ", "Slovaquie ", "Slovénie ", "Suède"])
+
+codes = np.array(["DE", "AT", "BE", "BG", "CY", "HR", "DK", "ES", "EE",
+	"FI", "FR", "EL", "HU", "IE", "IT", "LV", "LT", "LU",
+	"MT", "NL", "PL", "PT", "CZ", "RO", "GB", "SK", "SI", "SE"])
+
+TVA = np.array([19, 20, 21, 20, 19, 25, 25, 21, 20,
+	24, 20, 23, 27, 23, 22, 21, 21, 17,
+	18, 21, 23, 23, 21, 24, 20, 22, 20, 25])
+
+for i in range(countries.size):
+	print(codes[i] + "\t" + str(TVA[i]) + "%\t" + countries[i])
